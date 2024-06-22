c2e078a5beee5bea16f5bf07f03a320397cc8279
Nahid-subi <scientisthassannahid@gmail.com>
2024-06-22 21:04:14 +0600

    url change in socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://real-time-chat-app-hk4d.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
