bf57554e192e1eeac6e5366b4a5da52eeaf306ba
Nahid-subi <scientisthassannahid@gmail.com>
2024-06-22 20:27:16 +0600

    all functionality added

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,10 +1,12 @@
 import { useAuthContext } from "../../context/AuthContext";
+import useListenMessages from "../../hooks/useListenMessages";
 import { extractTime } from "../../utils/extractTime";
 import useConversation from "../../zustand/useConversation";
 
 const Message = ({message}) => {
 
     const {authUser}= useAuthContext()
+    useListenMessages()
     const {selectedConversation} = useConversation()
     const formattedTime = extractTime(message.createdAt)
 
@@ -12,6 +14,9 @@ const Message = ({message}) => {
     const chatClassName = fromMe ? "chat chat-end" : "chat chat-start"
     const profilePic = fromMe ? authUser.profilePic: selectedConversation?.profilePic
     const bubbledBgColor = fromMe ? "bg-blue-500" : "";
+    const shakeClass = message.shouldShake ? "shake" : ""
+
+
     return (
         <div className={`chat ${chatClassName}`}>
             <div className="chat-image avatar">
@@ -19,7 +24,7 @@ const Message = ({message}) => {
                     <img src={profilePic} alt="avatar" />
                 </div>
             </div>
-            <div className={`chat-bubble text-white ${bubbledBgColor} pb-2`}>
+            <div className={`chat-bubble text-white ${bubbledBgColor} ${shakeClass} pb-2`}>
                 {message?.message}
             </div>
             <div className="chat-footer text-xs opacity-50 gap-1 items-center">{formattedTime}</div>
