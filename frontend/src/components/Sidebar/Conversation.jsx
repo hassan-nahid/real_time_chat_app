620dec4da63e56c9df9444b8da27458c57586e09
Nahid-subi <scientisthassannahid@gmail.com>
2024-06-24 12:51:45 +0600

    a bug fixed

--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -11,19 +11,18 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 	return (
 		<>
 			<div
-				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+				className={`flex gap-1 items-center hover:bg-sky-500 rounded pr-5 py-1 cursor-pointer
 				${isSelected ? "bg-sky-500" : ""}
 			`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
 				<div className={`avatar ${isOnline ? "online" : ""}`}>
-					<div className='w-12 rounded-full'>
+					<div className='w-10 rounded-full'>
 						<img src={conversation.profilePic} alt='user avatar' />
 					</div>
 				</div>
-
-				<div className='flex flex-col flex-1'>
-					<div className='flex gap-3 justify-between'>
+				<div className='flex flex-col flex-1 mx-2'>
+					<div className='flex gap-1 justify-between '>
 						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
 						<span className='text-xl'>{emoji}</span>
 					</div>
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
 	return (
-		<div className='p-4 flex flex-col'>
+		<div className='flex flex-col'>
 			<SearchInput />
 			<div className='divider px-3'></div>
 			<Conversations />
